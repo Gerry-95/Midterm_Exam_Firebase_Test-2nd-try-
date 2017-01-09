5ec3c7c678cc88a47740d6e4b7005cf2fd704efa
Gerry-95 <scott.rusk@greenwichschools.org>
2017-01-09 13:23:31 -0500

    Add files via upload

--- js/My Test.js
+++ js/My Test.js
@@ -39,7 +39,7 @@ var answerArray = [
     ["-4", "3", "4", "2"],
     ["2x" + strsup3.sup() + " + x" + strsup2.sup() + " + 6x - 3", "2x" + strsup3.sup() + " - x" + strsup2.sup() + " + 5x - 3", "2x" + strsup3.sup() + " - x" + strsup2.sup() + " + 6x - 3", "2x" + strsup2.sup() + " + x" + strsup2.sup() + " + 6x - 3"],
     ["x(x + 2)(x - 2)/2x(x + 5)", "(x + 2)(x - 2)/2(x - 5)", "x(x" + strsup2.sup() + " - 4)/2x(x + 5)", "x(x" + strsup2.sup() + " + 4)/2x(x + 5)"],
-    ["as x -> infinity f(x) -> infinity; as x -> -infinity f(x) -> infinity", "as x -> infinity f(x) -> infinity; as x -> -infinity f(x) -> -infinity", "as x -> infinity f(x) -> -infinity; as x -> -infinity f(x) -> -infinity",  "as x -> infinity f(x) -> -infinity; as x -> -infinity f(x) -> infinity"],
+    ["as x nears infinity, f(x) nears infinity; as x nears -infinity, f(x) nears infinity", "as x nears infinity, f(x) nears infinity; as x nears -infinity, f(x) nears -infinity", "as x nears infinity, f(x) nears -infinity; as x nears -infinity, f(x) nears -infinity",  "as x nears infinity, f(x) nears -infinity; as x nears -infinity, f(x) nears infinity"],
     ["-12i - 5", "13 + 12i", "13", "-5 - 12i"],
     ["x = +/- 2i, x = - 1/2, x = 1", "x = - 2i, x = -1/2, x = 1, x = 2", "x = +/-2i, x = 1/2, x = 1", "x = +/- 2i, x = +/- 1"],
     ["(-infinity, infinity)", "(-infinity, 0)U(0, 1/4)", "(-infinity, 0)U(1/4, infinity)", "(0, 1/4)U(1/4, infinity)"],
