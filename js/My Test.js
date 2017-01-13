9e11acd131414713c7a5c75f71d68118d65affe8
Gerry-95 <scott.rusk@greenwichschools.org>
2017-01-12 22:15:59 -0500

    Add files via upload

--- js/My Test.js
+++ js/My Test.js
@@ -236,8 +236,7 @@ firebase.database().ref('/responses/').once('value').then(function(snapshot) {
         drawChart(outputArray);
         $("questionInfo").show();
         $("question1").show();
-        $("text1").html(
-            "" + (document.getElementById("questionText").innerHTML = questionArray[0]) + "<br>Your Answer: " + selectedAnswers[0] + "<br>Correct Answer: " + correctAnswers[0])
+        $("text1").html("" + (document.getElementById("questionText").innerHTML = questionArray[0]) + "<br>Your Answer: " + selectedAnswers[0] + "<br>Correct Answer: " + correctAnswers[0])
         $("advice1").html("get good")
         };
     });
